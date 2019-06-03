f06157f3e0945421be31f049f3affdf4eeae1d5b
Benjamin GONIN <benjamin.gonin@seloger.com>
2019-06-03 17:20:22 +0200

    Creating a Apollo Client, but we have a problem...

--- app/src/main/java/me/benju/apollotest/MainActivity.kt
+++ app/src/main/java/me/benju/apollotest/MainActivity.kt
@@ -2,12 +2,22 @@ package me.benju.apollotest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.apollographql.apollo.ApolloClient
 
 class MainActivity : AppCompatActivity() {
+
+    private val BASE_URL = "server.com/graphql"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val okHttpClient = OkHttpClient.Builder().build() // Problem =  Unresolved reference: OkHttpClient
+
+        val apolloClient = ApolloClient.builder()
+            .serverUrl(BASE_URL)
+            .okHttpClient(okHttpClient) // Problem here : Cannot access class 'okhttp3.OkHttpClient'. Check your module classpath for missing or conflicting dependencies
+            .build()
     }
 
 }
